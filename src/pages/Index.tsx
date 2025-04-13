c785518e9ec613022a20b072a8fc5ee2b25bd1f9
mohil c <176016365+Mohilc@users.noreply.github.com>
2025-04-13 09:02:23 +0530

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -37,7 +37,7 @@ const sustainabilityImages = [
     caption: "Working with local communities for sustainable economic growth"
   },
   {
-    url: "https://images.unsplash.com/photo-1564419429742-c6d342cebf01?auto=format&fit=crop&w=1200&q=80",
+    url: "https://plus.unsplash.com/premium_photo-1681140560925-a50f402b8525?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     alt: "Plant a tree program",
     caption: "One purchase = One tree planted in deforested areas"
   },
