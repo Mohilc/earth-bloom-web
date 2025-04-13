9933093fafd5b720f678fcc9e605721fb0a65624
mohil c <176016365+Mohilc@users.noreply.github.com>
2025-04-13 08:52:37 +0530

    Update Product.tsx

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -21,7 +21,7 @@ const productImages = [
     alt: "Eco-friendly packaging",
   },
   {
-    url: "https://images.unsplash.com/photo-1580910051073-3b7b3cf6ab2f?auto=format&fit=crop&w=1200&q=80",
+    url: "https://plus.unsplash.com/premium_photo-1737073520183-4f1b9ce340db?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     alt: "Product in use",
   },
   {
